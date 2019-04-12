1e868e16177fb12bad916ebc666f5c1164d1c5cb
Phuc V. Nguyen <phuc.vinh@outlook.com>
2019-04-13 02:18:51 +0700

    updated code

---
+++ concat.py
@@ -0,0 +1,17 @@
+
+
+"" + ""
+username = ""
+
+print("hello, welcome to the game " + username)
+
+name = ""
+name += ""
+
+number = 10
+number += 2
+number -= 3
+
+number *= 4
+number /= 3
+print(number)
---
+++ interpolate.py
@@ -0,0 +1,6 @@
+
+x = 10
+
+formatt = f"i've told you {x}"
+
+print(formatt)
