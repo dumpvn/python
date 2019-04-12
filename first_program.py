1112c722f4156109817605b6253309c662739395
Phuc V. Nguyen <phuc.vinh@outlook.com>
2019-04-13 00:58:56 +0700

    updated code

--- first_program.py
+++ first_program.py
@@ -1,1 +1,38 @@
 print("hello")
+print(3+6)
+
+# comment
+print(int("20"))
+# integer 4, 57, -10
+# 
+# floating point 6.1 1.33333 0.0
+# 
+print(type(9))
+print(type(9.0))
+print(type("phuc"))
+
+str(3)
+print(3 + 3.66) # result is float
+
+
+# + addition
+# subtraction -
+# multiplication
+# ** exp
+# %
+# //
+
+40-4.5
+print(1/2)
+print(type(1/2))
+print(1/3)
+
+# PEMDAS
+
+
+
+
+
+
+
+
