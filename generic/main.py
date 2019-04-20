dc0bec49b5ebd857d3fe0d9f56fc6a155b88e7ae
Phuc V. Nguyen <phuc.vinh@outlook.com>
2019-04-20 14:15:47 +0700

    updated code

--- generic/main.py
+++ generic/main.py
@@ -4,8 +4,19 @@
 main module for testing python packages
 """
 
-from logger.custom_logger import MyLogger
-
-
-if __name__ == "__main__":
+def TestCustomLogger():
+    from logger.custom_logger import MyLogger
     MyLogger.info("system starting....")
+
+def TestSimpleLogger():
+    from logger.simple_log import get_logger
+    logger = get_logger(__name__)
+    logger.debug("executing test case")
+    logger.info("this is just a simple logger")
+    logger.warning("nearly finish")
+    logger.error("faked error")
+    
+if __name__ == "__main__":
+    TestCustomLogger()
+    TestSimpleLogger()
+    
