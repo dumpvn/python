d5cae7f928f172dd4b68b6a3f4abacc2c88c2055
Phuc V. Nguyen <phuc.vinh@outlook.com>
2019-04-20 12:49:45 +0700

    updated code

---
+++ generic/main.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+"""
+main module for testing python packages
+"""
+
+from logger.custom_logger import MyLogger
+
+
+if __name__ == "__main__":
+    MyLogger.info("system starting....")
